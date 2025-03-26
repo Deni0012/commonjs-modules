c58db090192f6c5678e590cb2b48732a036cb142
Deni0012 <deni.dg00@icloud.com>
2025-03-26 15:43:08 +0100

    add: creazione function FullName

---
+++ names.js
@@ -0,0 +1,6 @@
+// Crea una funzione che accetta due parametri: firstName, lastName.
+function generateFullName (firstName, lastName){
+    return {firstName, lastName};
+}
+
+// La funzione dovrebbe restituire un oggetto con le proprietà firstName e lastName.
