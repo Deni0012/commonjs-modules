4a96c8e75a6481ac70708e899da7377f10cab44b
Deni0012 <deni.dg00@icloud.com>
2025-03-26 15:46:08 +0100

    fix: correzione della function

--- names.js
+++ names.js
@@ -1,6 +1,9 @@
 // Crea una funzione che accetta due parametri: firstName, lastName.
-function generateFullName (firstName, lastName){
-    return {firstName, lastName};
+function FullName (firstName, lastName){
+    return {
+        firstName, 
+        lastName
+    }
 }
 
 // La funzione dovrebbe restituire un oggetto con le proprietà firstName e lastName.
